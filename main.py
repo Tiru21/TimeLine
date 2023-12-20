5b0f1ccdd24c53f5f639e70a1b34175613f57194
Oleg1000-7 <oalzeg09@gmail.com>
2023-12-20 19:57:56 +0300

    huy

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("time_line.html", )
+def main():
+    return render_template("empty.html")
+
+@app.route("/history")
+def history():
+    return render_template("time_line.html")
+
+@app.route("/theory")
+def theory():
+    return render_template("empty.html")
+
+@app.route("/questions")
+def questions():
+    return render_template("questions.html")
+
+@app.route("/autors")
+def autors():
+    return render_template("empty.html")
 
 
 if __name__ == "__main__":
